941c525e44ca57df49b8b169a91970feee99d33f
danger12001 <paul@projectcodex.co>
2016-02-24 11:00:00 +0200

    CSS and HTML Updated

--- script.js
+++ script.js
@@ -3,27 +3,33 @@
   return Math.floor(Math.random() * 9) + 1;
   }
   // Function to print to the divs
-  function divPrint(){
+  function divPrintR1(){
     var num1 = document.getElementById('num1');
     num1.innerHTML = randomNumber();
     var num2 = document.getElementById('num2');
     num2.innerHTML = randomNumber();
     var num3 = document.getElementById('num3');
-    num3.innerHTML = randomnumber();
-    var num4 = document.getElementById('num4');
-    num4.innerHTML = randomnumber();
-    var num5 = document.getElementById('num5');
-    num5.innerHTML = randomnumber();
-    var num6 = document.getElementById('num6');
-    num6.innerHTML = randomnumber();
-    var num7 = document.getElementById('num7');
-    num7.innerHTML = randomnumber();
-    var num8 = document.getElementById('num8');
-    num8.innerHTML = randomnumber();
-    var num9 = document.getElementById('num9');
-    num9.innerHTML = randomnumber();
+    num3.innerHTML = randomNumber();
   }
+    function divPrintR2(){
+      var num4 = document.getElementById('num4');
+      num4.innerHTML = randomNumber();
+      var num5 = document.getElementById('num5');
+      num5.innerHTML = randomNumber();
+      var num6 = document.getElementById('num6');
+      num6.innerHTML = randomNumber();
+    }
+      function divPrintR3(){
+        var num7 = document.getElementById('num7');
+        num7.innerHTML = randomNumber();
+        var num8 = document.getElementById('num8');
+        num8.innerHTML = randomNumber();
+        var num9 = document.getElementById('num9');
+        num9.innerHTML = randomNumber();
+      }
 // Random Number Test
 console.log(randomNumber());
 //Calling of divPrint
-divPrint();
+divPrintR1();
+divPrintR2();
+divPrintR3();
