375ed89980b661bd994a4ad7b834f1059d63aaf5
danger12001 <paul@projectcodex.co>
2016-02-25 14:30:55 +0200

    Almost Finished

--- script.js
+++ script.js
@@ -15,49 +15,356 @@
 var numbers = document.querySelectorAll("numberBlock");
 
   // Function to print to the divs
-  function divPrint(){
+  function divPrint()
+  {
     num1.innerHTML = randomNumber();
     num2.innerHTML = randomNumber();
     num3.innerHTML = randomNumber();
-      num4.innerHTML = randomNumber();
-      num5.innerHTML = randomNumber();
-      num6.innerHTML = randomNumber();
-        num7.innerHTML = randomNumber();
-        num8.innerHTML = randomNumber();
-        num9.innerHTML = randomNumber();
-      }
-  // highlight Function
-highlight = function(){
-  var message = document.getElementById("MessageBox");
+    num4.innerHTML = randomNumber();
+    num5.innerHTML = randomNumber();
+    num6.innerHTML = randomNumber();
+    num7.innerHTML = randomNumber();
+    num8.innerHTML = randomNumber();
+    num9.innerHTML = randomNumber();
+  }
+      Row1Match = function(){
+        var message = document.getElementById("MessageBox");
+        if (num1.innerHTML === num2.innerHTML){
+          num3.classList.remove("highlight");
+          num4.classList.remove("highlight");
+          num5.classList.remove("highlight");
+          num6.classList.remove("highlight");
+          num7.classList.remove("highlight");
+          num8.classList.remove("highlight");
+          num9.classList.remove("highlight");
+          num1.classList.add("highlight");
+          num2.classList.add("highlight");
+          message.innerHTML = "A1 and A2 have matched";
+        }
+      else if (num1.innerHTML === num3.innerHTML){
+          num2.classList.remove("highlight");
+          num4.classList.remove("highlight");
+          num5.classList.remove("highlight");
+          num6.classList.remove("highlight");
+          num7.classList.remove("highlight");
+          num8.classList.remove("highlight");
+          num9.classList.remove("highlight");
+          num1.classList.add("highlight");
+          num3.classList.add("highlight");
+          message.innerHTML = "A1 and A3 have matched";
+        }
+      else if (num2.innerHTML === num3.innerHTML){
+          num1.classList.remove("highlight");
+          num4.classList.remove("highlight");
+          num5.classList.remove("highlight");
+          num6.classList.remove("highlight");
+          num7.classList.remove("highlight");
+          num8.classList.remove("highlight");
+          num9.classList.remove("highlight");
+          num2.classList.add("highlight");
+          num3.classList.add("highlight");
+          message.innerHTML = "A1 and A2 have matched";
+        }
+      };
+      Row2Match = function(){
+        var message = document.getElementById("MessageBox");
+        if (num4.innerHTML === num5.innerHTML){
+          num3.classList.remove("highlight");
+          num1.classList.remove("highlight");
+          num2.classList.remove("highlight");
+          num6.classList.remove("highlight");
+          num7.classList.remove("highlight");
+          num8.classList.remove("highlight");
+          num9.classList.remove("highlight");
+          num4.classList.add("highlight");
+          num5.classList.add("highlight");
+          message.innerHTML = "B1 and B2 have matched";
+        }
+      else if (num4.innerHTML === num6.innerHTML){
+          num2.classList.remove("highlight");
+          num3.classList.remove("highlight");
+          num5.classList.remove("highlight");
+          num1.classList.remove("highlight");
+          num7.classList.remove("highlight");
+          num8.classList.remove("highlight");
+          num9.classList.remove("highlight");
+          num4.classList.add("highlight");
+          num6.classList.add("highlight");
+          message.innerHTML = "B1 and B3 have matched";
+        }
+      else if (num5.innerHTML === num6.innerHTML){
+          num1.classList.remove("highlight");
+          num4.classList.remove("highlight");
+          num2.classList.remove("highlight");
+          num3.classList.remove("highlight");
+          num7.classList.remove("highlight");
+          num8.classList.remove("highlight");
+          num9.classList.remove("highlight");
+          num5.classList.add("highlight");
+          num6.classList.add("highlight");
+          message.innerHTML = "B2 and B3 have matched";
+        }
+      };
+      Row3Match = function(){
+        var message = document.getElementById("MessageBox");
+        if (num7.innerHTML === num8.innerHTML){
+          num3.classList.remove("highlight");
+          num4.classList.remove("highlight");
+          num5.classList.remove("highlight");
+          num6.classList.remove("highlight");
+          num2.classList.remove("highlight");
+          num1.classList.remove("highlight");
+          num9.classList.remove("highlight");
+          num7.classList.add("highlight");
+          num8.classList.add("highlight");
+          message.innerHTML = "C1 and C2 have matched";
+        }
+      else if (num7.innerHTML === num9.innerHTML){
+          num2.classList.remove("highlight");
+          num4.classList.remove("highlight");
+          num5.classList.remove("highlight");
+          num6.classList.remove("highlight");
+          num1.classList.remove("highlight");
+          num8.classList.remove("highlight");
+          num3.classList.remove("highlight");
+          num7.classList.add("highlight");
+          num9.classList.add("highlight");
+          message.innerHTML = "A1 and A3 have matched";
+        }
+      else if (num8.innerHTML === num9.innerHTML){
+          num1.classList.remove("highlight");
+          num4.classList.remove("highlight");
+          num5.classList.remove("highlight");
+          num6.classList.remove("highlight");
+          num7.classList.remove("highlight");
+          num2.classList.remove("highlight");
+          num3.classList.remove("highlight");
+          num8.classList.add("highlight");
+          num9.classList.add("highlight");
+          message.innerHTML = "C2 and C3 have matched";
+        }
+      };
+      Col1Match = function(){
+        var message = document.getElementById("MessageBox");
+        if (num1.innerHTML === num4.innerHTML){
+          num3.classList.remove("highlight");
+          num2.classList.remove("highlight");
+          num5.classList.remove("highlight");
+          num6.classList.remove("highlight");
+          num2.classList.remove("highlight");
+          num1.classList.remove("highlight");
+          num9.classList.remove("highlight");
+          num1.classList.add("highlight");
+          num4.classList.add("highlight");
+          message.innerHTML = "A1 and B1 have matched";
+        }
+      else if (num1.innerHTML === num7.innerHTML){
+          num2.classList.remove("highlight");
+          num4.classList.remove("highlight");
+          num5.classList.remove("highlight");
+          num6.classList.remove("highlight");
+          num9.classList.remove("highlight");
+          num8.classList.remove("highlight");
+          num3.classList.remove("highlight");
+          num1.classList.add("highlight");
+          num7.classList.add("highlight");
+          message.innerHTML = "A1 and C1 have matched";
+        }
+      else if (num4.innerHTML === num7.innerHTML){
+          num1.classList.remove("highlight");
+          num3.classList.remove("highlight");
+          num5.classList.remove("highlight");
+          num6.classList.remove("highlight");
+          num8.classList.remove("highlight");
+          num2.classList.remove("highlight");
+          num3.classList.remove("highlight");
+          num7.classList.add("highlight");
+          num9.classList.add("highlight");
+          message.innerHTML = "B1 and C1 have matched";
+        }
+      };
+      Col2Match = function(){
+        var message = document.getElementById("MessageBox");
+        if (num2.innerHTML === num5.innerHTML){
+          num3.classList.remove("highlight");
+          num4.classList.remove("highlight");
+          num7.classList.remove("highlight");
+          num6.classList.remove("highlight");
+          num8.classList.remove("highlight");
+          num1.classList.remove("highlight");
+          num9.classList.remove("highlight");
+          num2.classList.add("highlight");
+          num5.classList.add("highlight");
+          message.innerHTML = "A2 and B2 have matched";
+        }
+      else if (num2.innerHTML === num8.innerHTML){
+          num1.classList.remove("highlight");
+          num4.classList.remove("highlight");
+          num5.classList.remove("highlight");
+          num6.classList.remove("highlight");
+          num9.classList.remove("highlight");
+          num7.classList.remove("highlight");
+          num3.classList.remove("highlight");
+          num2.classList.add("highlight");
+          num8.classList.add("highlight");
+          message.innerHTML = "A2 and C2 have matched";
+        }
+      else if (num5.innerHTML === num8.innerHTML){
+          num1.classList.remove("highlight");
+          num3.classList.remove("highlight");
+          num7.classList.remove("highlight");
+          num6.classList.remove("highlight");
+          num9.classList.remove("highlight");
+          num2.classList.remove("highlight");
+          num3.classList.remove("highlight");
+          num5.classList.add("highlight");
+          num8.classList.add("highlight");
+          message.innerHTML = "B2 and C2 have matched";
+        }
+      };
+      Col1Match = function(){
+        var message = document.getElementById("MessageBox");
+        if (num1.innerHTML === num4.innerHTML){
+          num3.classList.remove("highlight");
+          num2.classList.remove("highlight");
+          num5.classList.remove("highlight");
+          num6.classList.remove("highlight");
+          num2.classList.remove("highlight");
+          num1.classList.remove("highlight");
+          num9.classList.remove("highlight");
+          num1.classList.add("highlight");
+          num4.classList.add("highlight");
+          message.innerHTML = "A1 and B1 have matched";
+        }
+      else if (num1.innerHTML === num7.innerHTML){
+          num2.classList.remove("highlight");
+          num4.classList.remove("highlight");
+          num5.classList.remove("highlight");
+          num6.classList.remove("highlight");
+          num9.classList.remove("highlight");
+          num8.classList.remove("highlight");
+          num3.classList.remove("highlight");
+          num1.classList.add("highlight");
+          num7.classList.add("highlight");
+          message.innerHTML = "A1 and C1 have matched";
+        }
+      else if (num4.innerHTML === num7.innerHTML){
+          num1.classList.remove("highlight");
+          num3.classList.remove("highlight");
+          num5.classList.remove("highlight");
+          num6.classList.remove("highlight");
+          num8.classList.remove("highlight");
+          num2.classList.remove("highlight");
+          num3.classList.remove("highlight");
+          num7.classList.add("highlight");
+          num9.classList.add("highlight");
+          message.innerHTML = "B1 and C1 have matched";
+        }
+      };
+      Col2Match = function(){
+        var message = document.getElementById("MessageBox");
+        if (num2.innerHTML === num5.innerHTML){
+          num3.classList.remove("highlight");
+          num1.classList.remove("highlight");
+          num4.classList.remove("highlight");
+          num6.classList.remove("highlight");
+          num2.classList.remove("highlight");
+          num1.classList.remove("highlight");
+          num9.classList.remove("highlight");
+          num2.classList.add("highlight");
+          num5.classList.add("highlight");
+          message.innerHTML = "A2 and B2 have matched";
+        }
+      else if (num2.innerHTML === num8.innerHTML){
+          num1.classList.remove("highlight");
+          num4.classList.remove("highlight");
+          num5.classList.remove("highlight");
+          num6.classList.remove("highlight");
+          num9.classList.remove("highlight");
+          num7.classList.remove("highlight");
+          num3.classList.remove("highlight");
+          num2.classList.add("highlight");
+          num8.classList.add("highlight");
+          message.innerHTML = "A2 and C2 have matched";
+        }
+      else if (num5.innerHTML === num8.innerHTML){
+          num1.classList.remove("highlight");
+          num3.classList.remove("highlight");
+          num7.classList.remove("highlight");
+          num6.classList.remove("highlight");
+          num9.classList.remove("highlight");
+          num2.classList.remove("highlight");
+          num3.classList.remove("highlight");
+          num5.classList.add("highlight");
+          num8.classList.add("highlight");
+          message.innerHTML = "B2 and C2 have matched";
+        }
+      };
+      Col3Match = function(){
+        var message = document.getElementById("MessageBox");
+        if (num3.innerHTML === num6.innerHTML){
+          num2.classList.remove("highlight");
+          num1.classList.remove("highlight");
+          num4.classList.remove("highlight");
+          num5.classList.remove("highlight");
+          num2.classList.remove("highlight");
+          num1.classList.remove("highlight");
+          num9.classList.remove("highlight");
+          num3.classList.add("highlight");
+          num6.classList.add("highlight");
+          message.innerHTML = "A3 and B3 have matched";
+        }
+      else if (num3.innerHTML === num9.innerHTML){
+          num1.classList.remove("highlight");
+          num4.classList.remove("highlight");
+          num5.classList.remove("highlight");
+          num6.classList.remove("highlight");
+          num8.classList.remove("highlight");
+          num7.classList.remove("highlight");
+          num2.classList.remove("highlight");
+          num3.classList.add("highlight");
+          num9.classList.add("highlight");
+          message.innerHTML = "A3 and C3 have matched";
+        }
+      else if (num6.innerHTML === num9.innerHTML){
+          num1.classList.remove("highlight");
+          num3.classList.remove("highlight");
+          num7.classList.remove("highlight");
+          num5.classList.remove("highlight");
+          num8.classList.remove("highlight");
+          num2.classList.remove("highlight");
+          num3.classList.remove("highlight");
+          num6.classList.add("highlight");
+          num9.classList.add("highlight");
+          message.innerHTML = "B3 and C3 have matched";
+        }
+      };
 
-if (num1.innerHTML === num2.innerHTML){
-  num1.classList.add("highlight");
-  num2.classList.add("highlight");
-  message.innerHTML = "A1 and A2 have matched";
+
+
+//button eventListener
+function button(){
+  var button = document.getElementById("Refresh");
+ divPrint();
+ // highlight();
+ Row1Match();
+ Row2Match();
+ Row3Match();
+ Col1Match();
+ Col2Match();
+ Col3Match();
 }
-else if(num1.innerHTML === num3.innerHTML){
-  num1.classList.add("highlight");
-  num3.classList.add("highlight");
-  message.innerHTML = "A1 and A3 have matched";
-}
-else if(num2.innerHTML === num3.innerHTML){
-  num2.classList.add("highlight");
-  num3.classList.add("highlight");
-  message.innerHTML = "A2 and A3 have matched";
-}
+var El = document.getElementById("Refresh");
+El.addEventListener("click",button,true);
 
-};
-
-    // var mkDiv = function(){
-    //   document.createElement('Div');
-    // };
-    // mkDiv("Hnum1").classList.add("highlight");
-
-      // this.classList.toggle("highlight",num1 == num2 || num1 == num3 || num2 == num3);
-// Calling Highlight
-// highlight();
 // Random Number Test
 console.log(randomNumber());
 //Calling of divPrint
 divPrint();
-highlight();
+// highlight();
+Row1Match();
+Row2Match();
+Row3Match();
+Col1Match();
+Col2Match();
+Col3Match();
